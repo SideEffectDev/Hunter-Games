6eb61f44351ce2bced3d0778cbf11fa78cc8c796
SideEffect <xsidexeffect@gmail.com>
2013-09-29 18:24:58 -0700

    Code Restructure

--- src/me/sideeffect/huntergames/HunterGames.java
+++ src/me/sideeffect/huntergames/HunterGames.java
@@ -1,21 +1,21 @@
 package me.sideeffect.huntergames;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 
+import de.robingrether.idisguise.api.DisguiseAPI;
 import pw.ender.messagebar.MessageBarSetEvent;
 
 public class HunterGames extends JavaPlugin {
@@ -27,31 +27,32 @@ public class HunterGames extends JavaPlugin {
 	public static String P;
 	public static String Map;
 	public static String currentMap;
-	public static int timeVote;
 	public static int gameTime;
 	public int tLimit = 150;
 	public int lTime = 45;
-	public int timeLimit;
-	public int lobbyTime;
+	public static int timeLimit;
+	public static int lobbyTime;
+	public static DisguiseAPI api;
+	static FileManager settings = FileManager.getInstance();	
 	public void onDisable() {
-		Methods.printLine("Version 0.1 Disabled");
+		Methods.printLine("Version" + this.getDescription().getVersion() + "Disabled");
 		for(Player onlinePlayers : Bukkit.getServer().getOnlinePlayers()){
 			HunterGames.scoreboard.resetScores(onlinePlayers);
 		}
-		getConfig().set("currentArena", "Lobby");
+		settings.getData().set("currentArena", "Lobby");
 		PlayerListener.zombieList.clear();
 	}
 
 	public void onEnable() {
 
-		loadConfig();
+		loadConfigurationFiles();
 		for (Player onlinePlayers : Bukkit.getServer().getOnlinePlayers()) {
-			onlinePlayers.removePotionEffect(PotionEffectType.SPEED);
-			onlinePlayers.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
+			PlayerListener.removeEffects(onlinePlayers);	
 		}
-		Server server = getServer();
-		PluginManager manage1r = server.getPluginManager();
-		Methods.printLine("Version " + manage1r +  "Enabled");
+		api = getServer().getServicesManager().getRegistration(DisguiseAPI.class).getProvider();
+		settings.getData().set("currentArena", "Lobby");
+		PluginDescriptionFile file = this.getDescription();
+		Methods.printLine("Version " + file.getVersion() +  " Enabled");
 		registerEvents();
 		me = this;
 		Prefix = getConfig().getString("prefix");
@@ -64,14 +65,14 @@ public class HunterGames extends JavaPlugin {
 		tLimit = getConfig().getInt("General.timeLimit");
 		lobbyTime = lTime;
 		timeLimit = tLimit;
-		getCommand("create").setExecutor(new CreateCommand(this));
-		getCommand("setspawn").setExecutor(new SetspawnCommand(this));
-		getCommand("setlobby").setExecutor(new SetlobbyCommand(this));
+		getCommand("create").setExecutor(new CreateCommand());
+		getCommand("setspawn").setExecutor(new SetspawnCommand());
+		getCommand("setlobby").setExecutor(new SetlobbyCommand());
 		getCommand("help").setExecutor(new HelpCommand(this));
 		Bukkit.getServer().getScheduler()
 		.scheduleSyncRepeatingTask(this, new Runnable() {
 			public void run() {
-				Map = getConfig().getString("currentArena");
+				Map = settings.getData().getString("currentArena");
 				currentMap = Map;
 				String prefix = getConfig().getString("General.ScoreboardPrefix").replaceAll("&", "§");
 				HunterGames.objective.setDisplayName(prefix);
@@ -83,26 +84,38 @@ public class HunterGames extends JavaPlugin {
 					MessageBarSetEvent Msg = new MessageBarSetEvent(onlinePlayers);
 					Msg.setMessage(currentMap);
 				}
-				if (Game.lobbyStarted == false
-						&& Game.gameStarted == false
-						&& Bukkit.getServer().getOnlinePlayers().length >= 2) {
-					Game.startGame();
+				if(settings.getData().contains("Arenas")){
+					List<String> arenas = new ArrayList<String>();
+					for (String possibleArenas : settings.getData().getConfigurationSection("Arenas").getKeys(false))
+					{
+
+						if (settings.getData().contains("Arenas." + possibleArenas + ".zombies") && settings.getData().contains("Arenas." + possibleArenas + ".humans"))
+						{
+							arenas.add(possibleArenas);
+						}
+					}
+					if (Game.lobbyStarted == false
+							&& Game.gameStarted == false
+							&& Bukkit.getServer().getOnlinePlayers().length >= 2 && arenas.size() > 0 && settings.getData().contains("Lobby")) {
+						Game.startGame();
+					}
 				}
+
 
 			}
 		}, 0, 20);
 	}
 
-	public void loadConfig() {
-		if (!new File(getDataFolder(), "config.yml").exists())
-			saveDefaultConfig();
-		saveConfig();
-
+	public void loadConfigurationFiles() {
+		this.saveDefaultConfig();
+		settings.setup(this);
 	}
 
+
+
+
 	public void registerEvents() {
-		getServer().getPluginManager().registerEvents(new PlayerListener(this),
-				this);
+		Bukkit.getServer().getPluginManager().registerEvents(new PlayerListener(this),this);
 	}
 
 }
